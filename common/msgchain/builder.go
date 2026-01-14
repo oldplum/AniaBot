bc138c9e4a56a285611dc8ae7ac4a21d4a82d687
jeanhua <2207739460@qq.com>
2026-01-14 18:26:28 +0800

    fix: 修复全局messagebuilder造成的消息污染 feat: 添加合并转发消息构造器

--- common/msgchain/builder.go
+++ common/msgchain/builder.go
@@ -7,39 +7,60 @@ import (
 )
 
 // ---------消息构造入口开始---------
-type chainBuilder struct{}
-
-// Friend 私聊消息构造器
-func (c chainBuilder) Friend() FriendChainBuilder {
-	return &chain{}
-}
-
-// Group 群聊消息构造器
-func (c chainBuilder) Group() GroupChainBuilder {
-	return &chain{}
-}
-
-var Builder = chainBuilder{}
-
-// ---------消息构造入口结束---------
-
-type chain struct {
+type chainBuilder struct {
 	message []message.OB11Segment
 }
 
-type Chain interface {
-	GetMsg() []message.OB11Segment
+type forardChainBuilder struct {
+	message message.ForwardMessage
 }
 
-func (c *chain) Build() Chain {
+// Friend 私聊消息构造器
+func (c *chainBuilder) Friend() FriendChainBuilder {
+	return &chainBuilder{
+		message: make([]message.OB11Segment, 0),
+	}
+}
+
+// Group 群聊消息构造器
+func (c *chainBuilder) Group() GroupChainBuilder {
+	return &chainBuilder{
+		message: make([]message.OB11Segment, 0),
+	}
+}
+
+// Forward 合并转发消息构造器
+func (c *chainBuilder) Forward() ForwardChainBuilder {
+	return &forardChainBuilder{
+		message: message.ForwardMessage{
+			Prompt:  "聊天记录",
+			Summary: "聊天记录",
+			Source:  "聊天记录",
+		},
+	}
+}
+
+var Builder = &chainBuilder{}
+
+// ---------消息构造入口结束---------
+
+func (c *chainBuilder) Build() Chain {
 	return c
 }
 
-func (c *chain) GetMsg() []message.OB11Segment {
+func (fc *forardChainBuilder) Build() ForwardChain {
+	return fc
+}
+
+func (c *chainBuilder) GetMsg() []message.OB11Segment {
 	return c.message
 }
 
-func (c *chain) Mention(userId uint) {
+func (fc *forardChainBuilder) GetMsg() message.ForwardMessage {
+	return fc.message
+}
+
+func (c *chainBuilder) Mention(userId uint) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "at",
 		Data: map[string]interface{}{
@@ -48,7 +69,7 @@ func (c *chain) Mention(userId uint) {
 	})
 }
 
-func (c *chain) Text(text string) {
+func (c *chainBuilder) Text(text string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "text",
 		Data: map[string]interface{}{
@@ -57,7 +78,7 @@ func (c *chain) Text(text string) {
 	})
 }
 
-func (c *chain) Face(faceId uint) {
+func (c *chainBuilder) Face(faceId uint) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "face",
 		Data: map[string]interface{}{
@@ -66,7 +87,7 @@ func (c *chain) Face(faceId uint) {
 	})
 }
 
-func (c *chain) ImageUrl(url string) {
+func (c *chainBuilder) ImageUrl(url string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "image",
 		Data: map[string]interface{}{
@@ -76,7 +97,7 @@ func (c *chain) ImageUrl(url string) {
 	})
 }
 
-func (c *chain) ImageBase64(bs64code string) {
+func (c *chainBuilder) ImageBase64(bs64code string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "image",
 		Data: map[string]interface{}{
@@ -86,7 +107,7 @@ func (c *chain) ImageBase64(bs64code string) {
 	})
 }
 
-func (c *chain) ImageLocal(path string) {
+func (c *chainBuilder) ImageLocal(path string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "image",
 		Data: map[string]interface{}{
@@ -96,7 +117,7 @@ func (c *chain) ImageLocal(path string) {
 	})
 }
 
-func (c *chain) Reply(msgId uint) {
+func (c *chainBuilder) Reply(msgId uint) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "reply",
 		Data: map[string]interface{}{
@@ -105,7 +126,7 @@ func (c *chain) Reply(msgId uint) {
 	})
 }
 
-func (c *chain) RecordUrl(url string) {
+func (c *chainBuilder) RecordUrl(url string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "record",
 		Data: map[string]interface{}{
@@ -114,7 +135,7 @@ func (c *chain) RecordUrl(url string) {
 	})
 }
 
-func (c *chain) RecordLocal(path string) {
+func (c *chainBuilder) RecordLocal(path string) {
 	c.message = append(c.message, message.OB11Segment{
 		Type: "record",
 		Data: map[string]interface{}{
@@ -123,6 +144,23 @@ func (c *chain) RecordLocal(path string) {
 	})
 }
 
-func (c *chain) Raw(rawMsg []message.OB11Segment) {
+func (c *chainBuilder) Raw(rawMsg []message.OB11Segment) {
 	c.message = append(c.message, rawMsg...)
+}
+
+func (fc *forardChainBuilder) Message(userId uint, nickname string, c Chain) {
+	fc.message.Messages = append(fc.message.Messages,
+		message.NodeMsg{
+			Type: "node",
+			Data: struct {
+				UserId   uint                  "json:\"user_id\""
+				Nickname string                "json:\"nickname\""
+				Content  []message.OB11Segment "json:\"content\""
+			}{
+				UserId:   userId,
+				Nickname: nickname,
+				Content:  c.GetMsg(),
+			},
+		},
+	)
 }
