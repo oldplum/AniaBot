e324d3d316e94308f65dd1ac3501724664ef65a7
jeanhua <2207739460@qq.com>
2026-02-21 17:08:47 +0800

    refactor: 修改消息转换工具函数

--- common/model/message/transfer.go
+++ common/model/message/transfer.go
@@ -6,106 +6,182 @@ import (
 	"strconv"
 )
 
-type transferable interface {
-	TextMessage | FaceMessage | ImageMessage | MentionMessage |
-		ReplyMessage | VideoMessage | RecordMessage | JsonMessage |
-		MusicMessage | FileMessage | ForwardMessage
-}
+// ParseXXX函数，将OB11Segment转换为对应消息
 
-func TransferTo[T transferable](s OB11Segment, target *T) bool {
-	return transfer(s, target)
-}
-
-func transfer(s OB11Segment, target interface{}) (success bool) {
-	defer func() {
-		if r := recover(); r != nil {
-			success = false
-		}
-	}()
-	switch t := target.(type) {
-	case *TextMessage:
-		if s.Type == "text" {
-			t.Text = s.Data["text"].(string)
-			return t.Text != ""
-		}
-		return false
-	case *FaceMessage:
-		if s.Type == "face" {
-			if id, err := strconv.Atoi(s.Data["id"].(string)); err != nil {
-				return false
-			} else {
-				t.Id = id
-			}
-			return true
-		}
-		return false
-	case *ImageMessage:
-		if s.Type == "image" {
-			t.File, _ = s.Data["file"].(string)
-			t.Url, _ = s.Data["url"].(string)
-			return t.Url != ""
-		}
-		return false
-	case *MentionMessage:
-		if s.Type == "at" {
-			qq, _ := s.Data["qq"].(string)
-			if qq == "all" {
-				t.IsAll = true
-				return true
-			}
-			qqInt, _ := strconv.Atoi(qq)
-			t.QQ = uint(qqInt)
-			return qq != ""
-		}
-		return false
-	case *ReplyMessage:
-		if s.Type == "reply" {
-			id, _ := s.Data["id"].(string)
-			idInt, _ := strconv.Atoi(id)
-			t.Id = uint(idInt)
-			return id != ""
-		}
-		return false
-	case *VideoMessage:
-		if s.Type == "video" {
-			t.URL, _ = s.Data["url"].(string)
-			return t.URL != ""
-		}
-		return false
-	case *RecordMessage:
-		if s.Type == "record" {
-			t.URL, _ = s.Data["url"].(string)
-			return t.URL != ""
-		}
-		return false
-	case *JsonMessage:
-		if s.Type == "json" {
-			if err := json.Unmarshal([]byte(s.Data["data"].(string)), t); err != nil {
-				log.Println("error when json unmarshal: json message", err)
-				return false
-			}
-			return true
-		}
-		return false
-	case *MusicMessage:
-		if s.Type == "music" {
-			t.Title, _ = s.Data["title"].(string)
-			return t.Title != ""
-		}
-		return false
-	case *FileMessage:
-		if s.Type == "file" {
-			t.File, _ = s.Data["file"].(string)
-			return t.File != ""
-		}
-		return false
-	case *ForwardMessage:
-		if s.Type == "forward" {
-			t.Id = s.Data["id"].(string)
-			return t.Id != ""
-		}
-		return false
-	default:
+func ParseText(s OB11Segment, t *TextMessage) bool {
+	if s.Type != "text" {
 		return false
 	}
+
+	text, ok := s.Data["text"].(string)
+	if !ok || text == "" {
+		return false
+	}
+
+	t.Text = text
+	return true
+}
+
+func ParseFace(s OB11Segment, f *FaceMessage) bool {
+	if s.Type != "face" {
+		return false
+	}
+
+	idStr, ok := s.Data["id"].(string)
+	if !ok {
+		return false
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return false
+	}
+
+	f.Id = id
+	return true
+}
+
+func ParseImage(s OB11Segment, i *ImageMessage) bool {
+	if s.Type != "image" {
+		return false
+	}
+
+	url, ok := s.Data["url"].(string)
+	if !ok || url == "" {
+		return false
+	}
+
+	file, _ := s.Data["file"].(string)
+
+	i.Url = url
+	i.File = file
+	return true
+}
+
+func ParseMention(s OB11Segment, m *MentionMessage) bool {
+	if s.Type != "at" {
+		return false
+	}
+
+	qq, ok := s.Data["qq"].(string)
+	if !ok {
+		return false
+	}
+
+	if qq == "all" {
+		m.IsAll = true
+		return true
+	}
+
+	id, err := strconv.Atoi(qq)
+	if err != nil {
+		return false
+	}
+
+	m.QQ = uint(id)
+	return true
+}
+
+func ParseReply(s OB11Segment, r *ReplyMessage) bool {
+	if s.Type != "reply" {
+		return false
+	}
+
+	idStr, ok := s.Data["id"].(string)
+	if !ok {
+		return false
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return false
+	}
+
+	r.Id = uint(id)
+	return true
+}
+
+func ParseVideo(s OB11Segment, v *VideoMessage) bool {
+	if s.Type != "video" {
+		return false
+	}
+
+	url, ok := s.Data["url"].(string)
+	if !ok || url == "" {
+		return false
+	}
+
+	v.URL = url
+	return true
+}
+
+func ParseRecord(s OB11Segment, r *RecordMessage) bool {
+	if s.Type != "record" {
+		return false
+	}
+
+	url, ok := s.Data["url"].(string)
+	if !ok || url == "" {
+		return false
+	}
+
+	r.URL = url
+	return true
+}
+
+func ParseJson(s OB11Segment, j *JsonMessage) bool {
+	if s.Type != "json" {
+		return false
+	}
+
+	raw, ok := s.Data["data"].(string)
+	if !ok {
+		return false
+	}
+
+	if err := json.Unmarshal([]byte(raw), j); err != nil {
+		log.Println("json unmarshal error:", err)
+		return false
+	}
+	return true
+}
+
+func ParseMusic(s OB11Segment, m *MusicMessage) bool {
+	if s.Type != "music" {
+		return false
+	}
+	title, ok := s.Data["title"].(string)
+	if !ok || title == "" {
+		return false
+	}
+	m.Title = title
+	return true
+}
+
+func ParseFile(s OB11Segment, f *FileMessage) bool {
+	if s.Type != "file" {
+		return false
+	}
+
+	file, ok := s.Data["file"].(string)
+	if !ok || file == "" {
+		return false
+	}
+
+	f.File = file
+	return true
+}
+
+func ParseForward(s OB11Segment, f *ForwardMessage) bool {
+	if s.Type != "forward" {
+		return false
+	}
+
+	id, ok := s.Data["id"].(string)
+	if !ok || id == "" {
+		return false
+	}
+
+	f.Id = id
+	return true
 }
