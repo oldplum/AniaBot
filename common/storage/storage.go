49c2931c733c1b6c16327019a9717575df3bdd15
jeanhua <2207739460@qq.com>
2026-02-22 13:52:05 +0800

    feat: 添加storage的ttl选项

--- common/storage/storage.go
+++ common/storage/storage.go
@@ -1,13 +1,28 @@
 package storage
 
-import "context"
+import (
+	"context"
+	"time"
+)
+
+type Option func(*StorageConfig)
+
+type StorageConfig struct {
+	TTL time.Duration
+}
+
+func WithTTL(ttl time.Duration) Option {
+	return func(sc *StorageConfig) {
+		sc.TTL = ttl
+	}
+}
 
 type Storage interface {
 	GetString(ctx context.Context, key string) (string, bool)
-	SetString(ctx context.Context, key, val string) bool
+	SetString(ctx context.Context, key, val string, option ...Option) bool
 
 	Get(ctx context.Context, key string, out any) bool
-	Set(ctx context.Context, key string, val any) bool
+	Set(ctx context.Context, key string, val any, option ...Option) bool
 
 	Del(ctx context.Context, key string) bool
 	Clear(ctx context.Context) bool
