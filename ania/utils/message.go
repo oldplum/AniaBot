9f317f8a2138bb7dfb4217c48e65df1bf38ccd33
jeanhua <2207739460@qq.com>
2026-02-23 18:46:42 +0800

    fix: message utils断言安全问题

--- ania/utils/message.go
+++ ania/utils/message.go
@@ -8,21 +8,25 @@ import (
 	"github.com/jeanhua/AniaBot/common/model/message"
 )
 
-func ExtraMessageStr(msg message.Message) (text string, mention bool) {
+func ExtraMessageStr(msg message.Message) (string, bool) {
 	var builder strings.Builder
-	mention = false
+	mention := false
 	for _, m := range msg.Message {
 		switch m.Type {
 		case "text":
-			builder.WriteString(m.Data["text"].(string))
+			if text, ok := m.Data["text"].(string); ok {
+				builder.WriteString(text)
+			}
 		case "at":
-			if m.Type == "at" && m.Data["qq"].(string) == strconv.Itoa(int(msg.SelfId)) {
-				mention = true
+			if qq, ok := m.Data["qq"].(string); ok {
+				if qq == strconv.Itoa(int(msg.SelfId)) {
+					mention = true
+				}
 			}
 		}
 	}
-	text = strings.TrimSpace(builder.String())
-	return
+	text := strings.TrimSpace(builder.String())
+	return text, mention
 }
 
 func HasMention(msg message.Message) bool {
