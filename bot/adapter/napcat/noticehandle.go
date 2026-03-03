88adc32f891323dd4c9b9506a3d4f6c0eaad9169
jeanhua <2207739460@qq.com>
2026-03-03 23:31:04 +0800

    refactor(noticehandler): 重构通知处理逻辑为注册表模式
    将switch-case结构重构为注册表模式，提高代码可维护性和扩展性

--- bot/adapter/napcat/noticehandle.go
+++ bot/adapter/napcat/noticehandle.go
@@ -8,150 +8,197 @@ import (
 	"github.com/jeanhua/AniaBot/common/model/message"
 )
 
+type noticeParser func(data []byte) any
+type noticeHandler func(notice any, trigger adapter.TriggerWrapper)
+
+type noticeTypeRegistry struct {
+	parser  noticeParser
+	handler noticeHandler
+}
+
+var noticeRegistry = map[string]noticeTypeRegistry{
+	"group_upload": {
+		parser: func(data []byte) any {
+			var notice message.GroupUploadNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupUpload != nil {
+				trigger.OnGroupUpload(notice.(message.GroupUploadNotice))
+			}
+		},
+	},
+	"group_admin": {
+		parser: func(data []byte) any {
+			var notice message.GroupAdminNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupAdmin != nil {
+				trigger.OnGroupAdmin(notice.(message.GroupAdminNotice))
+			}
+		},
+	},
+	"group_decrease": {
+		parser: func(data []byte) any {
+			var notice message.GroupDecreaseNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupDecrease != nil {
+				trigger.OnGroupDecrease(notice.(message.GroupDecreaseNotice))
+			}
+		},
+	},
+	"group_increase": {
+		parser: func(data []byte) any {
+			var notice message.GroupIncreaseNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupIncrease != nil {
+				trigger.OnGroupIncrease(notice.(message.GroupIncreaseNotice))
+			}
+		},
+	},
+	"group_ban": {
+		parser: func(data []byte) any {
+			var notice message.GroupBanNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupBan != nil {
+				trigger.OnGroupBan(notice.(message.GroupBanNotice))
+			}
+		},
+	},
+	"friend_add": {
+		parser: func(data []byte) any {
+			var notice message.FriendAddNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnFriendAdd != nil {
+				trigger.OnFriendAdd(notice.(message.FriendAddNotice))
+			}
+		},
+	},
+	"group_recall": {
+		parser: func(data []byte) any {
+			var notice message.GroupRecallNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupRecall != nil {
+				trigger.OnGroupRecall(notice.(message.GroupRecallNotice))
+			}
+		},
+	},
+	"friend_recall": {
+		parser: func(data []byte) any {
+			var notice message.FriendRecallNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnFriendRecall != nil {
+				trigger.OnFriendRecall(notice.(message.FriendRecallNotice))
+			}
+		},
+	},
+	"poke": {
+		parser: func(data []byte) any {
+			var notice message.PokeNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnPoke != nil {
+				trigger.OnPoke(notice.(message.PokeNotice))
+			}
+		},
+	},
+	"lucky_king": {
+		parser: func(data []byte) any {
+			var notice message.LuckyKingNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnLuckyKing != nil {
+				trigger.OnLuckyKing(notice.(message.LuckyKingNotice))
+			}
+		},
+	},
+	"honor": {
+		parser: func(data []byte) any {
+			var notice message.HonorNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnHonor != nil {
+				trigger.OnHonor(notice.(message.HonorNotice))
+			}
+		},
+	},
+	"group_msg_emoji_like": {
+		parser: func(data []byte) any {
+			var notice message.GroupMsgEmojiLikeNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupMsgEmojiLike != nil {
+				trigger.OnGroupMsgEmojiLike(notice.(message.GroupMsgEmojiLikeNotice))
+			}
+		},
+	},
+	"essence": {
+		parser: func(data []byte) any {
+			var notice message.EssenceNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnEssence != nil {
+				trigger.OnEssence(notice.(message.EssenceNotice))
+			}
+		},
+	},
+	"group_card": {
+		parser: func(data []byte) any {
+			var notice message.GroupCardNotice
+			json.Unmarshal(data, &notice)
+			return notice
+		},
+		handler: func(notice any, trigger adapter.TriggerWrapper) {
+			if trigger.OnGroupCard != nil {
+				trigger.OnGroupCard(notice.(message.GroupCardNotice))
+			}
+		},
+	},
+}
+
 func handleNotice(trigger adapter.TriggerWrapper, noticeType string, data []byte) {
-	switch noticeType {
-	case "group_upload":
-		var notice message.GroupUploadNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_upload]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupUpload != nil {
-			trigger.OnGroupUpload(notice)
-		}
-
-	case "group_admin":
-		var notice message.GroupAdminNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_admin]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupAdmin != nil {
-			trigger.OnGroupAdmin(notice)
-		}
-
-	case "group_decrease":
-		var notice message.GroupDecreaseNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_decrease]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupDecrease != nil {
-			trigger.OnGroupDecrease(notice)
-		}
-
-	case "group_increase":
-		var notice message.GroupIncreaseNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_increase]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupIncrease != nil {
-			trigger.OnGroupIncrease(notice)
-		}
-
-	case "group_ban":
-		var notice message.GroupBanNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_ban]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupBan != nil {
-			trigger.OnGroupBan(notice)
-		}
-
-	case "friend_add":
-		var notice message.FriendAddNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[friend_add]错误: ", err.Error())
-			return
-		}
-		if trigger.OnFriendAdd != nil {
-			trigger.OnFriendAdd(notice)
-		}
-
-	case "group_recall":
-		var notice message.GroupRecallNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_recall]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupRecall != nil {
-			trigger.OnGroupRecall(notice)
-		}
-
-	case "friend_recall":
-		var notice message.FriendRecallNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[friend_recall]错误: ", err.Error())
-			return
-		}
-		if trigger.OnFriendRecall != nil {
-			trigger.OnFriendRecall(notice)
-		}
-
-	case "poke":
-		var notice message.PokeNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[poke]错误: ", err.Error())
-			return
-		}
-		if trigger.OnPoke != nil {
-			trigger.OnPoke(notice)
-		}
-
-	case "lucky_king":
-		var notice message.LuckyKingNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[lucky_king]错误: ", err.Error())
-			return
-		}
-		if trigger.OnLuckyKing != nil {
-			trigger.OnLuckyKing(notice)
-		}
-
-	case "honor":
-		var notice message.HonorNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[honor]错误: ", err.Error())
-			return
-		}
-		if trigger.OnHonor != nil {
-			trigger.OnHonor(notice)
-		}
-
-	case "group_msg_emoji_like":
-		var notice message.GroupMsgEmojiLikeNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_msg_emoji_like]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupMsgEmojiLike != nil {
-			trigger.OnGroupMsgEmojiLike(notice)
-		}
-
-	case "essence":
-		var notice message.EssenceNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[essence]错误: ", err.Error())
-			return
-		}
-		if trigger.OnEssence != nil {
-			trigger.OnEssence(notice)
-		}
-
-	case "group_card":
-		var notice message.GroupCardNotice
-		if err := json.Unmarshal(data, &notice); err != nil {
-			log.Println("解析消息通知事件[group_card]错误: ", err.Error())
-			return
-		}
-		if trigger.OnGroupCard != nil {
-			trigger.OnGroupCard(notice)
-		}
-
-	default:
+	registry, ok := noticeRegistry[noticeType]
+	if !ok {
 		log.Println("未知的通知类型: ", noticeType)
 		return
 	}
+
+	notice := registry.parser(data)
+	if notice == nil {
+		log.Printf("解析通知事件[%s]错误\n", noticeType)
+		return
+	}
+
+	registry.handler(notice, trigger)
 }
