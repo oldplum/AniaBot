002a16cbffd7781f31026a3e88e0b919311d6245
jeanhua <2207739460@qq.com>
2026-03-07 11:59:24 +0800

    feat(urlparser): 添加内容缓存功能以减少重复请求
    实现URL解析结果的缓存机制，使用storage存储已处理内容并设置5分钟TTL
    添加上下文超时处理，优化错误日志信息

--- custom/plugins/urlparser/work.go
+++ custom/plugins/urlparser/work.go
@@ -2,10 +2,12 @@ package urlparser
 
 import (
 	"context"
+	"time"
 
 	"github.com/jeanhua/AniaBot/common/bot"
 	"github.com/jeanhua/AniaBot/common/model/message"
 	"github.com/jeanhua/AniaBot/common/msgchain"
+	"github.com/jeanhua/AniaBot/common/storage"
 	"github.com/tmc/langchaingo/llms"
 )
 
@@ -15,15 +17,33 @@ type work struct {
 	MsgID   message.QID
 }
 
+const (
+	cacheTTL   = time.Minute * 5
+	ctxTimeout = time.Minute * 5
+)
+
 func (p *URLParserPlugin) workLoop(bot bot.Bot) {
 	for w := range p.pendding {
-		content, err := fetchPageContent(context.Background(), p.token, w.URL)
+		ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+		defer cancel()
+
+		cache, ok := p.Storage.GetString(ctx, "cache:"+w.URL)
+		if ok {
+			p.Logger.Info("发送缓存内容", "groupId", w.GroupID, "content", cache)
+			builder := msgchain.Builder().Group()
+			builder.Reply(w.MsgID)
+			builder.Text(cache)
+			bot.SendGroupMsg(w.GroupID, builder.Build())
+			continue
+		}
+
+		content, err := fetchPageContent(ctx, p.token, w.URL)
 		if err != nil {
 			p.Logger.Info("fetchPageContent failed", "url", w.URL, "err", err)
 			continue
 		}
 
-		resp, err := p.llm.GenerateContent(context.Background(), []llms.MessageContent{
+		resp, err := p.llm.GenerateContent(ctx, []llms.MessageContent{
 			llms.TextParts(llms.ChatMessageTypeSystem, "你是一个网页内容提炼员，你的任务是从网页内容中提取相关信息，100字左右。"),
 			llms.TextParts(llms.ChatMessageTypeHuman, content),
 		}, llms.WithMaxTokens(300))
@@ -31,10 +51,14 @@ func (p *URLParserPlugin) workLoop(bot bot.Bot) {
 			p.Logger.Info("GenerateContent failed", "url", w.URL, "err", err)
 			continue
 		}
-		p.Logger.Info("[发->群 %d] %s", "groupId", w.GroupID, "content", resp.Choices[0].Content)
+
+		result := resp.Choices[0].Content
+		p.Storage.SetString(ctx, "cache:"+w.URL, result, storage.WithTTL(cacheTTL))
+
+		p.Logger.Info("发送提炼内容", "groupId", w.GroupID, "content", result)
 		builder := msgchain.Builder().Group()
 		builder.Reply(w.MsgID)
-		builder.Text(resp.Choices[0].Content)
+		builder.Text(result)
 		bot.SendGroupMsg(w.GroupID, builder.Build())
 	}
 }
